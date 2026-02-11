2d3a1dd18aaaa6805a49aaae364a68d93a77958c
ixann-ui <isannzero7@gmail.com>
2026-02-11 11:01:11 +0700

    deploy railway

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+const mysql = require("mysql2");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend aktif 🚀");
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Server jalan di port " + PORT);
+db.connect((err) => {
+  if (err) {
+    console.log("Database error:", err);
+  } else {
+    console.log("Database connected 🚀");
+  }
 });
