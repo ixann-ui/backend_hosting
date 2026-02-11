82c34079bd7273e7154c01d7f213ffa54d124e06
ixann-ui <isannzero7@gmail.com>
2026-02-11 11:05:34 +0700

    deploy

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const mysql = require("mysql2");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend aktif 🚀");
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log("Database error:", err);
-  } else {
-    console.log("Database connected 🚀");
-  }
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server jalan di port " + PORT);
 });
